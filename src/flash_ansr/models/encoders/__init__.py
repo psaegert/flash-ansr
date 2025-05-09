620792fd5e31f0bf0c3cea97713bd8b2be614982
psaegert <paulsaegert@protonmail.com>
2025-05-09 15:47:23 +0200

    Remove experimental imports

--- src/flash_ansr/models/encoders/__init__.py
+++ src/flash_ansr/models/encoders/__init__.py
@@ -1,6 +1,2 @@
 from .pre_encoder import PreEncoder
 from .set_transformer import SetTransformer, MAB, PMA, ISAB, SAB
-from .set_transformer2 import SetTransformer2, MABpp, PMApp, ISABpp, SABpp, SetNorm
-from .alternating_set_transformer import AlternatingSetTransformer
-from .flat_set_transformer import FlatSetTransformer
-from .set_transformer_1 import SetTransformer_1, MAB_1, PMA_1, ISAB_1, SAB_1
