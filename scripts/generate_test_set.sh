b5f107b1d381fbc7d317c02a25ceb9e767f4f9bc
psaegert <paulsaegert@protonmail.com>
2025-10-03 20:46:36 +0200

    Add v20.0-60M config and test set configs

--- scripts/generate_test_set.sh
+++ scripts/generate_test_set.sh
@@ -8,4 +8,4 @@ else
     CONFIG=$1
 fi
 
-flash_ansr generate-skeleton-pool -c {{ROOT}}/configs/${CONFIG}/skeleton_pool.yaml -e "{{ROOT}}/configs/test_set/simplipy_engine.yaml" -o {{ROOT}}/data/ansr-data/test_set/${CONFIG}/skeleton_pool -s 5000 -v
+flash_ansr generate-skeleton-pool -c {{ROOT}}/configs/test_set/${CONFIG}/skeleton_pool.yaml -o {{ROOT}}/data/ansr-data/test_set/${CONFIG}/skeleton_pool -s 1000 -v
