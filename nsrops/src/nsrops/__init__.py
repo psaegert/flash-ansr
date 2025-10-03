3343185eab17a2a167256f627467567c6f5684f3
psaegert <paulsaegert@protonmail.com>
2025-10-03 19:07:47 +0200

    remove obsolete nsrops package, use simplipy instead

--- nsrops/src/nsrops/__init__.py
+++
@@ -1,5 +0,0 @@
-from .power import *  # NOQA: F401 F403
-from .add import *  # NOQA: F401 F403
-from .multiply import *  # NOQA: F401 F403
-from .chebyshev import *  # NOQA: F401 F403
-from .bessel import *  # NOQA: F401 F403
