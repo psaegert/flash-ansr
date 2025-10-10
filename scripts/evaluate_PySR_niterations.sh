c66b4c068be52398ae1432caa1f8f146c4c8d50a
psaegert <paulsaegert@protonmail.com>
2025-10-10 15:39:16 +0200

    disable gradient checkpointing for v21.0-370M

--- scripts/evaluate_PySR_niterations.sh
+++ scripts/evaluate_PySR_niterations.sh
@@ -11,17 +11,17 @@ fi
 # 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768
 
 niterations_values=(
-    1 2 4 8 16 32 64
+    1 2 4 8 16 32 64 128 256
 )
 
 for niterations in "${niterations_values[@]}"; do
     output_dir="{{ROOT}}/results/evaluation/PySR_niterations/evaluation_niterations_${niterations}"
 
     flash_ansr evaluate-pysr \
-        -c "{{ROOT}}/configs/PySR_niterations/evaluation_niterations_${niterations}.yaml" \
+        -c "{{ROOT}}/configs/evaluation/PySR_niterations/evaluation_niterations_${niterations}.yaml" \
         -d "{{ROOT}}/data/ansr-data/test_set/$TEST_SET/dataset.yaml" \
         -e "dev_7-3" \
-        -n 512 \
+        -n 2048 \
         -o "$output_dir/$TEST_SET.pickle" \
         -v
 done
