ef81df131426c0b23185793dac73bde31b30e839
psaegert <paulsaegert@protonmail.com>
2025-11-14 15:56:47 +0100

    Set number of workers according to OMP_NUM_THREADS

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,27 @@
-# !/bin/bash
+#!/bin/bash
+
+set -euo pipefail
 
 # Check if an argument was passed
 if [ $# -eq 0 ]; then
-    echo "Usage: run.sh <CONFIG>"
+    echo "Usage: train.sh <CONFIG>"
     exit 1
 else
     CONFIG=$1
 fi
 
-flash_ansr train -c {{ROOT}}/configs/${CONFIG}/train.yaml -o {{ROOT}}/models/ansr-models/${CONFIG} -v -ci 250000 -vi 10000
+cmd=(
+    flash_ansr train
+    -c "{{ROOT}}/configs/${CONFIG}/train.yaml"
+    -o "{{ROOT}}/models/ansr-models/${CONFIG}"
+    -v
+    -ci 250000
+    -vi 10000
+)
+
+if [ -n "${OMP_NUM_THREADS:-}" ]; then
+    echo "Using OMP_NUM_THREADS=${OMP_NUM_THREADS}"
+    cmd+=(-w "${OMP_NUM_THREADS}")
+fi
+
+"${cmd[@]}"
