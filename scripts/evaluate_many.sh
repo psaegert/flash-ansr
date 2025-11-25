7792aaac9c66138dc05d7d05c0b872fa154d9c12
psaegert <paulsaegert@protonmail.com>
2025-11-25 22:18:53 +0100

    Fix evaluation config paths in scripts/evaluate_many.sh

--- scripts/evaluate_many.sh
+++ scripts/evaluate_many.sh
@@ -1,4 +1,4 @@
-flash_ansr evaluate-run -c configs/evaluation/v23.0-3M_fastsrb_long.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/v23.0-20M_fastsrb_long.yaml -v
+flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_fastsrb_long.yaml -v
+flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_fastsrb_long.yaml -v
 python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_fastsrb_long.yaml  -v
-flash_ansr evaluate-run -c configs/evaluation/nesymres_fastsrb_long.yaml -v
+flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_fastsrb_long.yaml -v
