f3d3729ce121e9b349c4df14ef165176e335812a
psaegert <paulsaegert@protonmail.com>
2025-11-26 15:17:39 +0100

    Update evaluation script

--- scripts/evaluate_many.sh
+++ scripts/evaluate_many.sh
@@ -1,3 +1,7 @@
+flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_fastsrb.yaml -v
+flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_fastsrb.yaml -v
+flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_v23_val.yaml -v
+
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_fastsrb_long.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_fastsrb_long.yaml -v
 python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_fastsrb_long.yaml  -v
