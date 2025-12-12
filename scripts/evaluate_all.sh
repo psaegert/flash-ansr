d0144d2e382ee2e427925bf1fcc24d88ae676807
psaegert <paulsaegert@protonmail.com>
2025-12-12 11:08:09 +0100

    Merge evaluation configs

--- scripts/evaluate_all.sh
+++ scripts/evaluate_all.sh
@@ -5,23 +5,9 @@ flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M_fastsrb.yaml -v
 python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_fastsrb.yaml  -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_fastsrb.yaml -v
 
-# FastSRB Long
-flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_fastsrb_long.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_fastsrb_long.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M_fastsrb_long.yaml -v
-python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_fastsrb_long.yaml  -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_fastsrb_long.yaml -v
-
 # v23_val
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_v23_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_v23_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M_v23_val.yaml -v
 python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_v23_val.yaml  -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_v23_val.yaml -v
-
-# v23_val Long
-flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_v23_val_long.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_v23_val_long.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M_v23_val_long.yaml -v
-python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_v23_val_long.yaml  -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_v23_val_long.yaml -v
