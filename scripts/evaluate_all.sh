8dcf8c4002e607cf13a49bfe3b011a15ad9f64cf
psaegert <paulsaegert@protonmail.com>
2026-01-10 16:58:59 +0100

    Add e2e to evaluation scripts

--- scripts/evaluate_all.sh
+++ scripts/evaluate_all.sh
@@ -1,9 +1,8 @@
-# FastSRB
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_fastsrb.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_fastsrb.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M_fastsrb.yaml -v
 python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_fastsrb.yaml  -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_fastsrb.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/skeleton_pool_fastsrb.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/brute_force_fastsrb.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/e2e_fastsrb.yaml -v
+# flash_ansr evaluate-run -c configs/evaluation/scaling/skeleton_pool_fastsrb.yaml -v
+# flash_ansr evaluate-run -c configs/evaluation/scaling/brute_force_fastsrb.yaml -v
