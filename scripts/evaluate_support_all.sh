64381b447a77d5a6d222268e55f077403c8d9eab
psaegert <paulsaegert@protonmail.com>
2026-01-16 00:34:45 +0100

    update scripts

--- scripts/evaluate_support_all.sh
+++ scripts/evaluate_support_all.sh
@@ -1,8 +1,9 @@
-flash_ansr evaluate-run -c configs/evaluation/support_sweep/v23.0-3M_fastsrb_support.yaml -v
+# flash_ansr evaluate-run -c configs/evaluation/support_sweep/v23.0-3M_fastsrb_support.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/support_sweep/v23.0-20M_fastsrb_support.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/support_sweep/v23.0-120M_fastsrb_support.yaml -v
-python scripts/evaluate_PySR.py  -c configs/evaluation/support_sweep/pysr_fastsrb_support.yaml  -v
-flash_ansr evaluate-run -c configs/evaluation/support_sweep/nesymres_fastsrb_support.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/support_sweep/e2e_fastsrb_support.yaml -v
+flash_ansr evaluate-run -c configs/evaluation/support_sweep/v23.0-1B_fastsrb_support.yaml -v
+# python scripts/evaluate_PySR.py  -c configs/evaluation/support_sweep/pysr_fastsrb_support.yaml  -v
+# flash_ansr evaluate-run -c configs/evaluation/support_sweep/nesymres_fastsrb_support.yaml -v
+# flash_ansr evaluate-run -c configs/evaluation/support_sweep/e2e_fastsrb_support.yaml -v
 # flash_ansr evaluate-run -c configs/evaluation/support_sweep/skeleton_pool_fastsrb_support.yaml -v
 # flash_ansr evaluate-run -c configs/evaluation/support_sweep/brute_force_fastsrb_support.yaml -v
