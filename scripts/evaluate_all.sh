881daef1bf194c4e49952a588fff9facfccd074f
psaegert <paulsaegert@protonmail.com>
2026-02-15 21:11:52 +0100

    Comment out low compute configs

--- scripts/evaluate_all.sh
+++ scripts/evaluate_all.sh
@@ -13,9 +13,9 @@ flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-3M_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-20M_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-1B_val.yaml -v
-python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_val.yaml  -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_val.yaml -v
-flash_ansr evaluate-run -c configs/evaluation/scaling/e2e_val.yaml -v
+# python scripts/evaluate_PySR.py  -c configs/evaluation/scaling/pysr_val.yaml  -v
+# flash_ansr evaluate-run -c configs/evaluation/scaling/nesymres_val.yaml -v
+# flash_ansr evaluate-run -c configs/evaluation/scaling/e2e_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/skeleton_pool_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M-ablation-1_val.yaml -v
 flash_ansr evaluate-run -c configs/evaluation/scaling/v23.0-120M-ablation-2_val.yaml -v
